290f9b2b19aa6fc913c602eccf7d971047e55751
Diego Corredor <206846973@uniparks.com>
2026-07-28 12:39:43 -0500

    test: add path and query parameter validations

--- src/test/java/api/tests/GetPostTest.java
+++ src/test/java/api/tests/GetPostTest.java
@@ -8,6 +8,10 @@ import io.restassured.http.ContentType;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import org.testng.Assert;
 
 public class GetPostTest {
@@ -22,8 +26,9 @@ public class GetPostTest {
         Response response =
             given()
                     .log().all()
+                    .pathParam("postId", 1)
             .when()
-                    .get("/posts/1")
+                    .get("/posts/{postId}")
             .then()
                 .log().all()
                 .statusCode(200)
@@ -49,5 +54,63 @@ public class GetPostTest {
         );
 
         System.out.println("Post Title: " + title);
+    }
+
+    @Test
+    public void shouldReturnNotFoundForNonExistingPost() {
+        given()
+                .log().all()
+                .pathParam("postId", 9999)
+                .when()
+                .get("/posts/{postId}")
+                .then()
+                .log().all()
+                .statusCode(404)
+                .contentType(ContentType.JSON)
+                .body("$", anEmptyMap());
+    }
+
+    @Test
+    public void shouldGetPostsByUserId() {
+        given()
+                .log().all()
+                .queryParam("userId", 1)
+                .when()
+                .get("/posts")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", greaterThan(0))
+                .body("userId", everyItem(equalTo(1)));
+    }
+
+    @Test
+    public void shouldReturnEmptyListForNonExistingUserId() {
+        given()
+                .log().all()
+                .queryParam("userId", 9999)
+                .when()
+                .get("/posts")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", equalTo(0));
+    }
+
+    @Test
+    public void shouldGetPostsForUserTwo() {
+        given()
+                .log().all()
+                .queryParam("userId", 2)
+                .when()
+                .get("/posts")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", greaterThan(0))
+                .body("userId", everyItem(equalTo(2)));
     }
 }
